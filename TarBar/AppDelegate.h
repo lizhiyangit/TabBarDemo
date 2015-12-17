d1976d723bbf9700b8fdf08dd35611bd67836c42
tldkj <tldkj@tldkjdemacbook-pro.local>
2015-12-17 15:31:54 +0800

    Initial Commit

---
+++ TarBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TarBar
+//
+//  Created by tldkj on 15/12/17.
+//  Copyright © 2015年 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TarBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TarBar
+//
+//  Created by tldkj on 15/12/17.
+//  Copyright © 2015年 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
